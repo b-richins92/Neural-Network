f4cbae82e2640b849b19b80bcf0a64bdbefa3a92
Raymondcanchola <36659241+Raymondcanchola@users.noreply.github.com>
2018-03-12 20:29:33 -0700

    Update data_part_iii.m

--- data_part_iii.m
+++ data_part_iii.m
@@ -10,38 +10,44 @@
 
 %First: Small Weights
 input = [1 2 3 4];
-weight = [.1 .2 .3 .4]/100000;
-activation_func(input,weight)
+weight = [.1 .2 .3 .4]/100;
+bias = [2;2;2;2];
+activation_func(input,weight,bias)
 %Ouput = 0.5000
 
 %Second: Medum Weights
 input = [1 2 3 4];
 weight = [.1 .2 .3 .4]/10;
-activation_func(input,weight)
+bias = [2;2;2;2];
+activation_func(input,weight,bias)
 %Ouput = 0.5744
 
 %Third: Large Weights
 input = [1 2 3 4];
 weight = [10 20 30 40];
-activation_func(input,weight)
+bias = [2;2;2;2];
+activation_func(input,weight,bias)
 %Ouput =  1
 
 %Fourth:Small Inputs
 input = [1 2 3 4];
 weight = [.1 .2 .3 .4]/100;
-activation_func(input,weight)
+bias = [2;2;2;2];
+activation_func(input,weight,bias)
 %Ouput = 0.5075
 
 %Fifth:Large Inputs
 input = [100 200 300 400];
 weight = [.1 .2 .3 .4]/100;
-activation_func(input,weight)
+bias = [2;2;2;2];
+activation_func(input,weight,bias)
 %Ouput = 0.9526
 
 %Fifth:Large Inputs
 input = [10000 20000 300000 400000];
 weight = [.1 .2 .3 .4]/100;
-activation_func(input,weight)
+bias = [2;2;2;2];
+activation_func(input,weight,bias)
 %Ouput = 1
 
 %Anaylsis:  By increasing the weights from small to large and leaving the 
@@ -51,4 +57,4 @@ activation_func(input,weight)
 %Functions: Different functions can be applied as the activated function
 %           such as F(net) = (tanh(net) + 1)/2 and the relu activation function:
 %           F(net) = max(0,net). Different functions increase and decrease
-%           accucracy.   
+%           accucracy. 
