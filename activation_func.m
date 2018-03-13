dbdc58374b907d1a20a34158d7e5eb7dadbaccfc
Raymondcanchola <36659241+Raymondcanchola@users.noreply.github.com>
2018-03-12 20:36:39 -0700

    Update activation_func.m

--- activation_func.m
+++ activation_func.m
@@ -6,9 +6,9 @@
 %The following function takes in Input=(O_1,...,O_n) and assigned
 %weights(W1,...,Wn) to find the output('neuron')
 
-function output = activation_func(input,weight)
+function output = activation_func(input,weight,bias)
      
-     net = sum(input*weight');
+     net = sum(input*weight'-bias);
      
      %Calling Sigmoid function
      output = sigmoid(net);
