0ce44a9abb48a2727b4f55f796da070a41921d11
b-richins92 <36213074+b-richins92@users.noreply.github.com>
2018-03-12 23:58:13 -0700

    Delete initializeweights.m

--- initializeweights.m
+++
@@ -1,6 +0,0 @@
-W1=rand(200,784); %weights between input layer and 1st hidden layer
-b1=rand(200,1);   %biases for first set of weights
-W2=rand(25,200);  %weights between 1st hidden layer and 2nd hidden layer
-b2=rand(25,1);   %biases for second set of weights
-W3=rand(10,25);   %Weights between 2nd hidden layer and output layer
-b3=rand(10,1);    %biases for third set of weights
